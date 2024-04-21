4214dfc1383c36de92d5963c1ad3c7ffd0c3a73a
Joel J George <joeljoby111@gmail.com>
2024-04-21 17:19:12 +0530

    feat(auth.js): added the user login

--- Api/Routes/auth.js
+++ Api/Routes/auth.js
@@ -39,3 +39,30 @@ router.post("/register", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await query("SELECT * FROM usertable WHERE username = $1", [
+    username,
+  ]);
+  if (user.length === 0) {
+    return res.status(400).json({ message: "User not found" });
+  }
+  if (await bcrypt.compare(password, user[0].password)) {
+    const accesstoken = jwt.sign(
+      {
+        id: user[0].id,
+        username: user[0].username,
+        name: user[0].name,
+      },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    return res
+      .cookie("token", accesstoken, {
+        httpOnly: true,
+      })
+      .json({ message: "Login successful" });
+  } else {
+    return res.status(400).json({ message: "Invalid password" });
+  }
+});
