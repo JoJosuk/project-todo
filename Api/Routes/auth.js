117938ed0cd3c3ca8f8aa02bfee5a782d25c5e3d
Joel J George <joeljoby111@gmail.com>
2024-04-21 17:17:32 +0530

    feat(auth.js): added the user registration

--- Api/Routes/auth.js
+++ Api/Routes/auth.js
@@ -1,4 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const { query } = require("../utils/db");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../..", ".env") });
 
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+  const { username, password, name } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const altuser = await query("SELECT * FROM usertable WHERE username = $1", [
+    username,
+  ]);
+  if (altuser.length > 0) {
+    return res.status(400).json({ message: "Username allready taken" });
+  }
+  try {
+    await query(
+      "INSERT INTO usertable (username, password, name) VALUES ($1, $2, $3)",
+      [username, hashedPassword, name]
+    );
+    const user = await query("SELECT * FROM usertable WHERE username = $1", [
+      username,
+    ]);
+    const accesstoken = jwt.sign(
+      {
+        id: user[0].id,
+        username: user[0].username,
+        name: user[0].name,
+      },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    return res
+      .cookie("token", accesstoken, {
+        httpOnly: true,
+      })
+      .json({ message: "User created" });
+  } catch (e) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
