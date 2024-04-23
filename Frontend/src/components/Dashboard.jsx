f95cc8b4b305cec23ddab98d5d8d7ce7283fafef
Joel J George <joeljoby111@gmail.com>
2024-04-23 21:31:54 +0530

    feat(dashboard.jsx): implemented delete

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -3,12 +3,24 @@ import axios from "axios";
 import Navigationbar from "./Navbar";
 import Projectview from "./Projectview";
 import { Button, useDisclosure } from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 
 export default function Dashboard() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [projectid, setProjectid] = useState("");
   const [projectTitle, setProjectTitle] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
+  const [reload, setReload] = useState(false);
+  const [projects, setProjects] = useState([]);
+  const [name, setName] = useState("");
+  const deleteProject = async (projectid) => {
+    const response = await axios.delete(`http://localhost:5000/project/`, {
+      data: { id: projectid },
+      withCredentials: true,
+    });
+    console.log(response.data);
+    setReload(!reload);
+  };
   const onOpenProject = async () => {
     const response = await axios.post(
       "http://localhost:5000/project/",
@@ -24,7 +36,7 @@ export default function Dashboard() {
     setProjectid(response.data.id);
     setProjectDescription(response.data.description);
     setProjectTitle(response.data.title);
-
+    setReload(!reload);
     onOpen();
   };
   useEffect(() => {
@@ -37,9 +49,7 @@ export default function Dashboard() {
       setName(response.data.name);
     };
     fetchData();
-  }, []);
-  const [projects, setProjects] = useState([]);
-  const [name, setName] = useState("");
+  }, [reload]);
 
   return (
     <div className="w-full min-h-screen bg-stone-900">
@@ -47,6 +57,53 @@ export default function Dashboard() {
       <div>hey</div>
       <div className="flex justify-center w-full">
         <div className="w-5/6">
+          <h1>Projects</h1>
+          <div className="flex flex-wrap gap-3 p-5">
+            {projects &&
+              projects.map((project, index) => {
+                return (
+                  <Card
+                    shadow="sm"
+                    key={index}
+                    isPressable
+                    onPress={() => console.log("item pressed")}
+                  >
+                    <CardBody className="p-0 overflow-visible">
+                      <Image
+                        shadow="sm"
+                        radius="lg"
+                        width="100%"
+                        alt={project.title}
+                        className="w-full object-cover h-[140px]"
+                        src="https://nextui.org/images/album-cover.png"
+                      />
+                    </CardBody>
+                    <CardFooter className="justify-between text-small">
+                      <b>{project.title}</b>
+                      <div
+                        className="p-1 text-red-500 rounded-sm hover:bg-red-200"
+                        onClick={() => deleteProject(project.id)}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                          />
+                        </svg>
+                      </div>
+                    </CardFooter>
+                  </Card>
+                );
+              })}
+          </div>
           <Button onPress={onOpenProject}>➕ Add Project</Button>
           <Projectview
             isOpen={isOpen}
--- Frontend/src/components/Projectview.jsx
+++ Frontend/src/components/Projectview.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { useDebounce } from "use-debounce";
 import {
   Modal,
   ModalContent,
@@ -12,6 +13,7 @@ import {
   Link,
 } from "@nextui-org/react";
 import { useEffect } from "react";
+import axios from "axios";
 export default function Projectview({
   isOpen,
   onOpenChange,
@@ -20,13 +22,55 @@ export default function Projectview({
   title,
   description,
 }) {
+  const [projectTitle, setProjectTitle] = useState();
+  const [debouncedTitle] = useDebounce(projectTitle, 1000);
+  const [projectDescription, setProjectDescription] = useState();
+  const [debouncedDescription] = useDebounce(projectDescription, 1000);
   useEffect(() => {
     setProjectTitle(title);
     setProjectDescription(description);
   }, [title, description]);
-  console.log("the id is ", id, title, description);
-  const [projectTitle, setProjectTitle] = useState();
-  const [projectDescription, setProjectDescription] = useState();
+  useEffect(() => {
+    if (debouncedTitle) {
+      axios
+        .put(
+          "http://localhost:5000/project/",
+          {
+            id: id,
+            title: projectTitle,
+            description: projectDescription,
+          },
+          { withCredentials: true }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
+    }
+  }, [debouncedTitle]);
+  useEffect(() => {
+    if (debouncedDescription) {
+      axios
+        .put(
+          "http://localhost:5000/project/",
+          {
+            id: id,
+            title: projectTitle,
+            description: projectDescription,
+          },
+          { withCredentials: true }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
+    }
+  }, [debouncedDescription]);
+
   return (
     <Modal
       isOpen={isOpen}
